494d342cbad2a6b0ff929a7702095ea409911179
khakis27 <davide.agostinelli@mail.polimi.it>
2022-11-23 11:09:54 +0100

    3

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 console.log("hello");
+
+let express = require("express");
+
+let app = express();
+
+let port = 3000;
+
+let server = app.listen(port);
+
+console.log('running server on http://localhost:' + port);
+
+app.use(express.static("public"))
